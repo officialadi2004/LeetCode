7c6c593784427b08e8070313e81da3e9d7a26c87
officialadi2004 <adityakachwaha9@gmail.com>
2026-09-24 14:36:33 +0530

    Time: 149 ms (75.79%), Space: 32.4 MB (93.68%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.py
@@ -1,25 +1,19 @@
 class Solution:
     def minOperations(self, grid, x):
-        nums = []
+        nums = [num for row in grid for num in row]
 
-        for row in grid:
-            for num in row:
-                nums.append(num)
-
-        # Check if conversion is possible
-        remainder = nums[0] % x
+        rem = nums[0] % x
 
         for num in nums:
-            if num % x != remainder:
+            if num % x != rem:
                 return -1
 
-        # Median minimizes total absolute difference
         nums.sort()
         median = nums[len(nums) // 2]
 
-        operations = 0
+        ans = 0
 
         for num in nums:
-            operations += abs(num - median) // x
+            ans += abs(num - median) // x
 
-        return operations
+        return ans
